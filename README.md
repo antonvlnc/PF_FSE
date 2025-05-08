c1fd806ad708804d879a9b8144157bbc09922c66
antonio_toledo <antonio.toledovlnc@gmail.com>
2025-05-07 20:35:40 -0600

    Inital Commit + ideas
    Initial code + comments about the future of the project

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PF_FSE
 Proyecto Final - Fundamentos de Sistemas Embebidos
+
+"Alimentador Automatizado para Gatos"
+
+Ejemplo de operación:
+
+- Selección del nombre del gato (?)
+- Selección de edad del gato
+- Selección de la hora a alimentar
+
+
+
+Imágenes de uso:
+
+Maqueta general
+*imagen del final del proyecto*
+
+Menú de selección
+*colocar imágenes*
+
---
+++ main.py
@@ -0,0 +1,18 @@
+#Programa unificado: Proyecto Final "Dispensador de comida para gatos" (sólo funciones de alto nivel)
+
+
+'''
+Pseudocódigo (hacer un diagrama de flujo y agregarlo al repo):
+
+Inicio
+- Compuerta cerrada / display LCD en main menu / plato sin comida. La botella con el alimento debe estar cerca del plato por
+    la comida que cae aunque el servo se active y se cierre rápido.
+
+Será en función del tiempo    
+
+Seleccion del tamaño del gato (Bebé, Adolescente, Adulto)
+- Bebé = 50g
+- Adolescente = 250g
+- Adulto = 400g
+
+'''
